25fff864f4fbafdbce071b6ece43f13a025b16ed
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2020-05-06 17:14:37 +0530

    Add files via upload

--- m1.js
+++ m1.js
@@ -6,6 +6,17 @@ $("#Arbtn").click(function(){
   $(".Ar").show();
 });
 
+$("#goBack").click(function(){
+  $(".head1").show();
+  $(".Singers").show();
+  $(".Ar").hide();
+});
+
+$("audio").bind("play",function (){
+  $("audio").not(this).each(function (index, audio) {
+    audio.pause();
+  });
+});
 
 
 });
