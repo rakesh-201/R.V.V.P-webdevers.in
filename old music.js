13a1219093191e0d2b235e4629dc74173b8858ad
vdmondkr2002 <64477617+vdmondkr2002@users.noreply.github.com>
2020-05-14 11:31:44 +0530

    Added Singers

--- old music.js
+++ old music.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $(".Md").hide();
+    $(".Kk").hide();
+    $(".Lm").hide();
+    $(".Ab").hide();
     $("#goback").hide();
     $("#Mdbtn").click(function(){
       $(".head1").hide();
@@ -7,12 +10,33 @@ $(document).ready(function(){
       $(".Md").show();
       $("#goback").show();
     });
+    $("#Lmbtn").click(function(){
+      $(".head1").hide();
+      $(".Singers").hide();
+      $(".Lm").show();
+      $("#goback").show();
+    })
+    $("#Kkbtn").click(function(){
+      $(".head1").hide();
+      $(".Singers").hide();
+      $(".Kk").show();
+      $("#goback").show();
+    });
+    $("#Abbtn").click(function(){
+      $(".head1").hide();
+      $(".Singers").hide();
+      $(".Ab").show();
+      $("#goback").show();
+    });
     $("#goback").click(function(e){
       $(this).hide();
       $(".Md").hide();
+      $(".Lm").hide();
+      $(".Kk").hide();
+      $(".Ab").hide();
       $(".head1").show();
       $(".Singers").show();
-    })
+    });
     $("audio").bind("play",function (){
       $("audio").not(this).each(function (index, audio) {
         audio.pause();
