417e1a44384a43d0208b5edfaf6fb0713ec10f9e
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2020-05-20 15:05:02 +0530

    added Music Album

--- m1.js
+++ m1.js
@@ -1,22 +1,55 @@
-$(document).ready(function(){
-$(".Ar").hide();
-$("#Arbtn").click(function(){
-  $(".head1").hide();
-  $(".Singers").hide();
-  $(".Ar").show();
-});
 
-$("#goBack").click(function(){
-  $(".head1").show();
-  $(".Singers").show();
-  $(".Ar").hide();
-});
+$(".album-poster").on("click",function(){
+  var dataSwitchId=$(this).attr("data-switch");
+  //console.log(dataSwitchId);
+   
+  ap.list.switch(dataSwitchId);
 
-$("audio").bind("play",function (){
-  $("audio").not(this).each(function (index, audio) {
-    audio.pause();
-  });
+  ap.play();
+  $("#aplayer").addClass("showPlayer");
 });
 
 
+
+
+
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+ listFolded:true,
+  audio: [{ 
+      name: 'Bekhayali',
+      artist: 'Arjit Singh',
+      url: 'Bekhayali.mp3',
+      cover: 'Bekhayali.jpg'
+  },{ 
+    name: 'Dil Hi Toh Hai',
+    artist: 'Arjit Singh',
+    url: 'Dil Hi Toh Hai.mp3',
+    cover: 'Dil Hi Toh Hai.jpg'
+},
+{ 
+  name: 'Sanam Re',
+  artist:'Arjit Singh',
+  url: 'Sanam Re.mp3',
+  cover: 'Sanam Re.jpg'
+},
+{ 
+  name: 'Khairiyat',
+  artist: 'Arjit Singh',
+  url: 'Khairiyat.mp3',
+  cover: 'Khairiyat.jpg'
+},
+{ 
+  name: 'Song name',
+  artist: 'artist',
+  url: 'Khairiyat.mp3',
+  cover: 'arjit.jpg'
+},
+{ 
+  name: 'Song name',
+  artist: 'artist',
+  url: 'Khairiyat.mp3',
+  cover: 'arjit.jpg'
+}
+]
 });
