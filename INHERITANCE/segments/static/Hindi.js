1e32cbc9e4ce8292c81ced7cba0e8f0ad5479863
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2020-05-27 16:43:12 +0530

    Updated  hindi .js  mp3 and jpg files

--- INHERITANCE/segments/static/Hindi.js
+++ INHERITANCE/segments/static/Hindi.js
@@ -23,32 +23,32 @@ const ap = new APlayer({
   }, {
     name: 'Ae Dil Hai Mushkil',
     artist: 'Arjit Singh',
-    url: 'Hindi songs/Ae Dil Hai Mushkil.mp3',
-    cover: 'Hindi songs/Ae Dil Hai Mushkil.jpg'
+    url: 'Hindi songs/Ae_Dil_Hai_Mushkil.mp3',
+    cover: 'Hindi songs/Ae_Dil_Hai_Mushkil.jpg'
   },
   {
     name: 'All Black',
     artist: 'Raftaar, Sukh-E',
-    url: 'Hindi songs/All Black.mp3',
-    cover: 'Hindi songs/All Black.jpg'
+    url: 'Hindi songs/All_Black.mp3',
+    cover: 'Hindi songs/All_Black.jpg'
   },
   {
     name: 'Ankhiyon Se Goli Maare',
     artist: 'Jaspender Narula, Sonu Nigam',
-    url: 'Hindi songs/Ankhiyon Se Goli Maare.mp3',
-    cover: 'Hindi songs/Ankhiyon Se Goli Maare.jpg'
+    url: 'Hindi songs/Ankhiyon_Se_Goli_Maare.mp3',
+    cover: 'Hindi songs/Ankhiyon_Se_Goli_Maare.jpg'
   },
   {
     name: 'Apna Time Aayega',
     artist: 'Ranveer Singh',
-    url: 'Hindi songs/Apna Time Aayega.mp3',
-    cover: 'Hindi songs/Apna Time Aayega.jpg'
+    url: 'Hindi songs/Apna_Time_Aayega.mp3',
+    cover: 'Hindi songs/Apna_Time_Aayega.jpg'
   },
   {
     name: 'Ban Ja Rani',
     artist: 'Guru Randhawa, Shraddha Vajpayee',
-    url: 'Hindi songs/Ban Ja Rani.mp3',
-    cover: 'Hindi songs/Ban Ja Rani.jpg'
+    url: 'Hindi songs/Ban_Ja_Rani.mp3',
+    cover: 'Hindi songs/Ban_Ja_Rani.jpg'
   },
   {
     name: 'Bekhayali',
@@ -65,14 +65,14 @@ const ap = new APlayer({
   {
     name: 'Bheegi Bheegi',
     artist: 'Neha Kakkar and Tony Kakkar',
-    url: 'Hindi songs/Bheegi Bheegi.mp3',
-    cover: 'Hindi songs/Bheegi Bheegi.jpg'
+    url: 'Hindi songs/Bheegi_Bheegi.mp3',
+    cover: 'Hindi songs/Bheegi_Bheegi.jpg'
   },
   {
     name: 'Bom Diggy Diggy',
     artist: 'Jasmine Walia and Zack Knight',
-    url: 'Hindi songs/Bom Diggy Diggy.mp3',
-    cover: 'Hindi songs/Bom Diggy Diggy.jpg'
+    url: 'Hindi songs/Bom_Diggy_Diggy.mp3',
+    cover: 'Hindi songs/Bom_Diggy_Diggy.jpg'
   },
   {
     name: 'Breathless',
@@ -83,38 +83,38 @@ const ap = new APlayer({
   {
     name: 'Chal Bombay',
     artist: 'Divine',
-    url: 'Hindi songs/Chal Bombay.mp3',
-    cover: 'Hindi songs/Chal Bombay.jpg'
+    url: 'Hindi songs/Chal_Bombay.mp3',
+    cover: 'Hindi songs/Chal_Bombay.jpg'
   },
   {
     name: 'Cheez Badi',
     artist: 'Neha Kakkar',
-    url: 'Hindi songs/Cheez Badi.mp3',
-    cover: 'Hindi songs/Cheez Badi.jpg'
+    url: 'Hindi songs/Cheez_Badi.mp3',
+    cover: 'Hindi songs/Cheez_Badi.jpg'
   },
   {
     name: 'Coca Cola',
     artist: 'Tony Kakkar',
-    url: 'Hindi songs/Coca Cola.mp3',
-    cover: 'Hindi songs/Coca Cola.jpg'
+    url: 'Hindi songs/Coca_Cola.mp3',
+    cover: 'Hindi songs/Coca_Cola.jpg'
   },
   {
     name: 'Daddy Mummy',
     artist: 'Devi Sri Prasad and M.M.Manasi',
-    url: 'Hindi songs/Daddy Mummy.mp3',
-    cover: 'Hindi songs/Daddy Mummy.jpg'
+    url: 'Hindi songs/Daddy_Mummy.mp3',
+    cover: 'Hindi songs/Daddy_Mummy.jpg'
   },
   {
     name: 'Das Bahane 2.0',
     artist: 'Vishal-Shekhar',
-    url: 'Hindi songs/Das Bahane 2.0.mp3',
-    cover: 'Hindi songs/Das Bahane 2.0.jpg'
+    url: 'Hindi songs/Das_Bahane 2.0.mp3',
+    cover: 'Hindi songs/Das_Bahane 2.0.jpg'
   },
   {
     name: 'Deewani Mastani',
     artist: 'Shreya Ghoshal',
-    url: 'Hindi songs/Deewani Mastani.mp3',
-    cover: 'Hindi songs/Deewani Mastani.jpg'
+    url: 'Hindi songs/Deewani_Mastani.mp3',
+    cover: 'Hindi songs/Deewani_Mastani.jpg'
   },
   {
     name: 'Dilbar',
@@ -125,14 +125,14 @@ const ap = new APlayer({
   {
     name: 'Dil abhi bhara nhi',
     artist: 'Jaspinder Narula and Shankar Mahadevan',
-    url: 'Hindi songs/Dil abhi bhara nhi.mp3',
-    cover: 'Hindi songs/Dil abhi bhara nhi.jpg'
+    url: 'Hindi songs/Dil_abhi_bhara_nhi.mp3',
+    cover: 'Hindi songs/Dil_abhi_bhara_nhi.jpg'
   },
   {
     name: 'Dil Hi Toh Hai',
     artist: 'Antara Mitra and Arijit Singh',
-    url: 'Hindi songs/Dil Hi Toh Hai.mp3',
-    cover: 'Hindi songs/Dil Hi Toh Hai.jpg'
+    url: 'Hindi songs/Dil_Hi_Toh_Hai.mp3',
+    cover: 'Hindi songs/Dil_Hi_Toh_Hai.jpg'
   },
   {
     name: 'FIRSE MACHAYENGE',
@@ -143,20 +143,20 @@ const ap = new APlayer({
   {
     name: 'Gali Gali.jpg',
     artist: 'Neha Kakkar',
-    url: 'Hindi songs/Gali Gali.mp3',
-    cover: 'Hindi songs/Gali Gali.jpg'
+    url: 'Hindi songs/Gali_Gali.mp3',
+    cover: 'Hindi songs/Gali_Gali.jpg'
   },
   {
     name: 'Garmi Song',
     artist: 'Badshah and Neah Kakkar',
-    url: 'Hindi songs/Garmi Song.mp3',
-    cover: 'Hindi songs/Garmi Song.jpg'
+    url: 'Hindi songs/Garmi_Song.mp3',
+    cover: 'Hindi songs/Garmi_Song.jpg'
   },
   {
     name: 'Ghammand Kar',
     artist: 'Parampara Thakur and Sachet Tandon',
-    url: 'Hindi songs/Ghammand Kar.mp3',
-    cover: 'Hindi songs/Ghammand Kar.jpg'
+    url: 'Hindi songs/Ghammand_Kar.mp3',
+    cover: 'Hindi songs/Ghammand_Kar.jpg'
   },
   {
     name: 'Ghunghroo',
@@ -167,38 +167,38 @@ const ap = new APlayer({
   {
     name: 'High Rated',
     artist: 'Gururandhawa',
-    url: 'Hindi songs/High Rated.mp3',
-    cover: 'Hindi songs/High Rated.jpg'
+    url: 'Hindi songs/High_Rated.mp3',
+    cover: 'Hindi songs/High_Rated.jpg'
   },
   {
     name: 'Ik Mulakat',
     artist: 'Altamash Faridi, Palak Muchhal and Meet Bros',
-    url: 'Hindi songs/Ik Mulakat.mp3',
-    cover: 'Hindi songs/Ik Mulakat.jpg'
+    url: 'Hindi songs/Ik_Mulakat.mp3',
+    cover: 'Hindi songs/Ik_Mulakat.jpg'
   },
   {
     name: 'Illegal Weapon 2.0',
     artist: 'Garry Sandhu and Jasmine Sandias',
-    url: 'Hindi songs/Illegal Weapon 2.0 .mp3',
-    cover: 'Hindi songs/Illegal Weapon 2.0 .jpg'
+    url: 'Hindi songs/Illegal_Weapon_2.0.mp3',
+    cover: 'Hindi songs/Illegal_Weapon_2.0.jpg'
   },
   {
     name: 'Ishare Tere',
     artist: 'Gururandhawa and Dhvani Bhanushali',
-    url: 'Hindi songs/Ishare Tere.mp3',
-    cover: 'Hindi songs/Ishare Tere.jpg'
+    url: 'Hindi songs/Ishare_Tere.mp3',
+    cover: 'Hindi songs/Ishare_Tere.jpg'
   },
   {
     name: 'Jo Bheji Thi Duaa',
     artist: 'Arijit Singh',
-    url: 'Hindi songs/Jo Bheji Thi Duaa.mp3',
-    cover: 'Hindi songs/Jo Bheji Thi Duaa.jpg'
+    url: 'Hindi songs/Jo_Bheji_Thi_Duaa.mp3',
+    cover: 'Hindi songs/Jo_Bheji_Thi_Duaa.jpg'
   },
   {
     name: 'Kabhi Jo Badal Barse',
     artist: 'Arijit Singh',
-    url: 'Hindi songs/Kabhi Jo Badal Barse.mp3',
-    cover: 'Hindi songs/Kabhi Jo Badal Barse.jpg'
+    url: 'Hindi songs/Kabhi_Jo_Badal_Barse.mp3',
+    cover: 'Hindi songs/Kabhi_Jo_Badal_Barse.jpg'
   },
   {
     name: 'Kamariya',
