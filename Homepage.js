715a527e55db0ecb60b69ec33cfc195d9d639bc7
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2020-06-03 19:19:05 +0530

    added designed home pages

--- Homepage.js
+++ Homepage.js
@@ -1,7 +1,30 @@
-$(documnent).ready(function(){
 
-    $(".navbar").click(function(){
-       document.write("This is navbar");
-       console.log("hi");
-    });
-});
+var images = [
+  "music.jpg","diary.jpg","news.jpg"
+];
+var slideText= [
+    "Let the Melody Go Into Your Soul", "Note and Review Your To Do List" , "Voice Controlled News Section"
+]
+var i=0;
+function slides (){
+document.getElementById("slideimage").src= images[i];
+if(i<(images.length-1)){
+    i++;
+}
+else{
+  i=0;  
+}
+}
+setInterval(slides,2000);
+
+var j=0;
+function slidetext (){
+document.getElementById("slideText").innerText= slideText[j];
+if(j<(slideText.length-1)){
+    j++;
+}
+else{
+  j=0;  
+}
+}
+setInterval(slidetext,2000);
