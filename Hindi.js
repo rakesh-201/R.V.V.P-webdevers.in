5a15eed6b3415b51945ecbea0d19c8e27ebea76e
prashanttb <64590462+prashanttb@users.noreply.github.com>
2020-06-13 23:51:06 -0700

    Add files via upload

--- Hindi.js
+++ Hindi.js
@@ -1,8 +1,7 @@
-
-$(".album-poster").on("click",function(){
-  var dataSwitchId=$(this).attr("data-switch");
+$(".album-poster").on("click", function () {
+  var dataSwitchId = $(this).attr("data-switch");
   //console.log(dataSwitchId);
-   
+
   ap.list.switch(dataSwitchId);
 
   ap.play();
@@ -15,197 +14,301 @@ $(".album-poster").on("click",function(){
 
 const ap = new APlayer({
   container: document.getElementById('aplayer'),
- listFolded:true,
-  audio: [{ 
-      name: 'Aankh Mare',
-      artist: 'Kumar Sanu, Mika Singh, Neha Kakkar',
-      url: 'Aankh Mare.mp3',
-      cover: 'Aankh Mare.jpg'
-  },{ 
+  listFolded: true,
+  audio: [{
+    name: 'Aankh Mare',
+    artist: 'Kumar Sanu, Mika Singh, Neha Kakkar',
+    url: 'static/Hindi_songs/Aankh_Mare.mp3',
+    cover: 'static/Hindi_songs/Aankh_Mare.jpg'
+  },
+  {
     name: 'Ae Dil Hai Mushkil',
     artist: 'Arjit Singh',
-    url: 'Ae Dil Hai Mushkil.mp3',
-    cover: 'Ae Dil Hai Mushkil.jpg'
-},
-{ 
-  name: 'All Black',
-  artist:'Raftaar, Sukh-E',
-  url: 'All Black.mp3',
-  cover: 'All Black.jpg'
-},
-{ 
-  name: 'Ankhiyon Se Goli Maare',
-  artist: 'Jaspender Narula, Sonu Nigam',
-  url: 'Ankhiyon Se Goli Maare.mp3',
-  cover: 'Ankhiyon Se Goli Maare.jpg'
-},
-{ 
-  name: 'Apna Time Aayega',
-  artist: 'Ranveer Singh',
-  url: 'Apna Time Aayega.mp3',
-  cover: 'Apna Time Aayega.jpg'
-},
-{ 
-  name: 'Ban Ja Rani',
-  artist: 'Guru Randhawa, Shraddha Vajpayee',
-  url: 'Ban Ja Rani.mp3',
-  cover: 'Ban Ja Rani.jpg'
-},
-{ 
-  name: 'Bekhayali',
-  artist: 'Arigit Singh',
-  url: 'Bekhayali.mp3',
-  cover: 'Bekhayali.jpg'
-},
-{ 
-  name: 'Bekhudi',
-  artist: 'Aditi Sharma and Darshan Raval',
-  url: 'Bekhudi.mp3',
-  cover: 'Bekhudi.jpg'
-},
-{ 
-  name: 'Bheegi Bheegi',
-  artist: 'Neha Kakkar and Tony Kakkar',
-  url: 'Bheegi Bheegi.mp3',
-  cover: 'Bheegi Bheegi.jpg'
-},
-{ 
-  name: 'Bom Diggy Diggy',
-  artist: 'Jasmine Walia and Zack Knight',
-  url: 'Bom Diggy Diggy.mp3',
-  cover: 'Bom Diggy Diggy.jpg'
-},
-{ 
-  name: 'Breathless',
-  artist: 'Shankar Mahadevan',
-  url: 'Breathless.mp3',
-  cover: 'Breathless.jpg'
-},
-{ 
-  name: 'Chal Bombay',
-  artist: 'Divine',
-  url: 'Chal Bombay.mp3',
-  cover: 'Chal Bombay.jpg'
-},
-{ 
-  name: 'Cheez Badi',
-  artist: 'Neha Kakkar',
-  url: 'Cheez Badi.mp3',
-  cover: 'Cheez Badi.jpg'
-},
-{ 
-  name: 'Coca Cola',
-  artist: 'Tony Kakkar',
-  url: 'Coca Cola.mp3',
-  cover: 'Coca Cola.jpg'
-},
-{ 
-  name: 'Daddy Mummy',
-  artist: 'Devi Sri Prasad and M.M.Manasi',
-  url: 'Daddy Mummy.mp3',
-  cover: 'Daddy Mummy.jpg'
-},
-{ 
-  name: 'Das Bahane 2.0',
-  artist: 'Vishal-Shekhar',
-  url: 'Das Bahane 2.0.mp3',
-  cover: 'Das Bahane 2.0.jpg'
-},
-{ 
-  name: 'Deewani Mastani',
-  artist: 'Shreya Ghoshal',
-  url: 'Deewani Mastani.mp3',
-  cover: 'Deewani Mastani.jpg'
-},
-{ 
-  name: 'Dilbar',
-  artist: 'Dhvani Bhanushali, Ikka Singh and Neha Kakkar',
-  url: 'Dilbar.mp3',
-  cover: 'Dilbar.jpg'
-},
-{ 
-  name: 'Dil abhi bhara nhi',
-  artist: 'Jaspinder Narula and Shankar Mahadevan',
-  url: 'Dil abhi bhara nhi,mp3',
-  cover: 'Dil abhi bhara nhi.jpg'
-},
-{ 
-  name: 'Dil Hi Toh Hai',
-  artist: 'Antara Mitra and Arijit Singh',
-  url: 'Dil Hi Toh Hai.mp3',
-  cover: 'Dil Hi Toh Hai.jpg'
-},
-{ 
-  name: 'FIRSE MACHAYENGE',
-  artist: 'EMIWAY BANTAI',
-  url: 'FIRSE MACHAYENGE.mp3',
-  cover: 'FIRSE MACHAYENGE.jpg'
-},
-{ 
-  name: 'Gali Gali.jpg',
-  artist: 'Neha Kakkar',
-  url: 'Gali Gali.mp3',
-  cover: 'Gali Gali.jpg'
-},
-{ 
-  name: 'Garmi Song',
-  artist: 'Badshah and Neah Kakkar',
-  url: 'Garmi Song.mp3',
-  cover: 'Garmi Song.jpg'
-},
-{ 
-  name: 'Ghammand Kar',
-  artist: 'Parampara Thakur and Sachet Tandon',
-  url: 'Ghammand Kar.mp3',
-  cover: 'Ghammand Kar.jpg'
-},
-{ 
-  name: 'Ghunghroo',
-  artist: 'Arijit Singh',
-  url: 'Ghunghroo.mp3',
-  cover: 'Ghunghroo.jpg'
-},
-{ 
-  name: 'High Rated',
-  artist: 'Gururandhawa',
-  url: 'High Rated.mp3',
-  cover: 'High Rated.jpg'
-},
-{ 
-  name: 'Ik Mulakat',
-  artist: 'Altamash Faridi, Palak Muchhal and Meet Bros',
-  url: 'Ik Mulakat.mp3',
-  cover: 'Ik Mulakat.jpg'
-},
-{ 
-  name: 'Illegal Weapon 2.0',
-  artist: 'Garry Sandhu and Jasmine Sandias',
-  url: 'Illegal Weapon 2.0 .mp3',
-  cover: 'Illegal Weapon 2.0 .jpg'
-},
-{ 
-  name: 'Ishare Tere',
-  artist: 'Gururandhawa and Dhvani Bhanushali',
-  url: 'Ishare Tere.mp3',
-  cover: 'Ishare Tere.jpg'
-},
-{ 
-  name: 'Jo Bheji Thi Duaa',
-  artist: 'Arijit Singh',
-  url: 'Jo Bheji Thi Duaa.mp3',
-  cover: 'Jo Bheji Thi Duaa.jpg'
-},
-{ 
-  name: 'Kabhi Jo Badal Barse',
-  artist: 'Arijit Singh',
-  url: 'Kabhi Jo Badal Barse.mp3',
-  cover: 'Kabhi Jo Badal Barse.jpg'
-},
-{ 
-  name: 'Kamariya',
-  artist: 'Aastha Gill, Divya Kumar and Sachin Sanghvi',
-  url: 'Kamariya.mp3',
-  cover: 'Kamariya.jpg'
-}
-]
+    url: 'static/Hindi_songs/Ae_Dil_Hai_Mushkil.mp3',
+    cover: 'static/Hindi_songs/Ae_Dil_Hai_Mushkil.jpg'
+  },
+  {
+    name: 'All Black',
+    artist: 'Raftaar, Sukh-E',
+    url: 'static/Hindi_songs/All_Black.mp3',
+    cover: 'static/Hindi_songs/All_Black.jpg'
+  },
+  {
+    name: 'Ankhiyon Se Goli Maare',
+    artist: 'Jaspender Narula, Sonu Nigam',
+    url: 'static/Hindi_songs/Ankhiyon_Se_Goli_Maare.mp3',
+    cover: 'static/Hindi_songs/Ankhiyon_Se_Goli_Maare.jpg'
+  },
+  {
+    name: 'Apna Time Aayega',
+    artist: 'Ranveer Singh',
+    url: 'static/Hindi_songs/Apna_Time_Aayega.mp3',
+    cover: 'static/Hindi_songs/Apna_Time_Aayega.jpg'
+  },
+  {
+    name: 'Ban Ja Rani',
+    artist: 'Guru Randhawa, Shraddha Vajpayee',
+    url: 'static/Hindi_songs/Ban_Ja_Rani.mp3',
+    cover: 'static/Hindi_songs/Ban_Ja_Rani.jpg'
+  },
+  {
+    name: 'Bekhayali',
+    artist: 'Arigit Singh',
+    url: 'static/Hindi_songs/Bekhayali.mp3',
+    cover: 'static/Hindi_songs/Bekhayali.jpg'
+  },
+  {
+    name: 'Bekhudi',
+    artist: 'Aditi Sharma and Darshan Raval',
+    url: 'static/Hindi_songs/Bekhudi.mp3',
+    cover: 'static/Hindi_songs/Bekhudi.jpg'
+  },
+  {
+    name: 'Bheegi Bheegi',
+    artist: 'Neha Kakkar and Tony Kakkar',
+    url: 'static/Hindi_songs/Bheegi_Bheegi.mp3',
+    cover: 'static/Hindi_songs/Bheegi_Bheegi.jpg'
+  },
+  {
+    name: 'Bom Diggy Diggy',
+    artist: 'Jasmine Walia and Zack Knight',
+    url: 'static/Hindi_songs/Bom_Diggy_Diggy.mp3',
+    cover: 'static/Hindi_songs/Bom_Diggy_Diggy.jpg'
+  },
+  {
+    name: 'Breathless',
+    artist: 'Shankar Mahadevan',
+    url: 'static/Hindi_songs/Breathless.mp3',
+    cover: 'static/Hindi_songs/Breathless.jpg'
+  },
+  {
+    name: 'Chal Bombay',
+    artist: 'Divine',
+    url: 'static/Hindi_songs/Chal_Bombay.mp3',
+    cover: 'static/Hindi_songs/Chal_Bombay.jpg'
+  },
+  {
+    name: 'Cheez Badi',
+    artist: 'Neha Kakkar',
+    url: 'static/Hindi_songs/Cheez_Badi.mp3',
+    cover: 'static/Hindi_songs/Cheez_Badi.jpg'
+  },
+  {
+    name: 'Coca Cola',
+    artist: 'Tony Kakkar',
+    url: 'static/Hindi_songs/Coca_Cola.mp3',
+    cover: 'static/Hindi_songs/Coca_Cola.jpg'
+  },
+  {
+    name: 'Daddy Mummy',
+    artist: 'Devi Sri Prasad and M.M.Manasi',
+    url: 'static/Hindi_songs/Daddy_Mummy.mp3',
+    cover: 'static/Hindi_songs/Daddy_Mummy.jpg'
+  },
+  {
+    name: 'Das Bahane 2.0',
+    artist: 'Vishal-Shekhar',
+    url: 'static/Hindi_songs/Das_Bahane 2.0.mp3',
+    cover: 'static/Hindi_songs/Das_Bahane 2.0.jpg'
+  },
+  {
+    name: 'Deewani Mastani',
+    artist: 'Shreya Ghoshal',
+    url: 'static/Hindi_songs/Deewani_Mastani.mp3',
+    cover: 'static/Hindi_songs/Deewani_Mastani.jpg'
+  },
+  {
+    name: 'Dilbar',
+    artist: 'Dhvani Bhanushali, Ikka Singh and Neha Kakkar',
+    url: 'static/Hindi_songs/Dilbar.mp3',
+    cover: 'static/Hindi_songs/Dilbar.jpg'
+  },
+  {
+    name: 'Dil abhi bhara nhi',
+    artist: 'Jaspinder Narula and Shankar Mahadevan',
+    url: 'static/Hindi_songs/Dil_abhi_bhara_nhi.mp3',
+    cover: 'static/Hindi_songs/Dil_abhi_bhara_nhi.jpg'
+  },
+  {
+    name: 'Dil Hi Toh Hai',
+    artist: 'Antara Mitra and Arijit Singh',
+    url: 'static/Hindi_songs/Dil_Hi_Toh_Hai.mp3',
+    cover: 'static/Hindi_songs/Dil_Hi_Toh_Hai.jpg'
+  },
+  {
+    name: 'FIRSE MACHAYENGE',
+    artist: 'EMIWAY BANTAI',
+    url: 'static/Hindi_songs/FIRSE MACHAYENGE.mp3',
+    cover: 'static/Hindi_songs/FIRSE MACHAYENGE.jpg'
+  },
+  {
+    name: 'Gali Gali.jpg',
+    artist: 'Neha Kakkar',
+    url: 'static/Hindi_songs/Gali_Gali.mp3',
+    cover: 'static/Hindi_songs/Gali_Gali.jpg'
+  },
+  {
+    name: 'Garmi Song',
+    artist: 'Badshah and Neah Kakkar',
+    url: 'static/Hindi_songs/Garmi_Song.mp3',
+    cover: 'static/Hindi_songs/Garmi_Song.jpg'
+  },
+  {
+    name: 'Ghammand Kar',
+    artist: 'Parampara Thakur and Sachet Tandon',
+    url: 'static/Hindi_songs/Ghammand_Kar.mp3',
+    cover: 'static/Hindi_songs/Ghammand_Kar.jpg'
+  },
+  {
+    name: 'Ghunghroo',
+    artist: 'Arijit Singh',
+    url: 'static/Hindi_songs/Ghunghroo.mp3',
+    cover: 'static/Hindi_songs/Ghunghroo.jpg'
+  },
+  {
+    name: 'High Rated',
+    artist: 'Gururandhawa',
+    url: 'static/Hindi_songs/High_Rated.mp3',
+    cover: 'static/Hindi_songs/High_Rated.jpg'
+  },
+  {
+    name: 'Ik Mulakat',
+    artist: 'Altamash Faridi, Palak Muchhal and Meet Bros',
+    url: 'static/Hindi_songs/Ik_Mulakat.mp3',
+    cover: 'static/Hindi_songs/Ik_Mulakat.jpg'
+  },
+  {
+    name: 'Illegal Weapon 2.0',
+    artist: 'Garry Sandhu and Jasmine Sandias',
+    url: 'static/Hindi_songs/Illegal_Weapon_2.0.mp3',
+    cover: 'static/Hindi_songs/Illegal_Weapon_2.0.jpg'
+  },
+  {
+    name: 'Ishare Tere',
+    artist: 'Gururandhawa and Dhvani Bhanushali',
+    url: 'static/Hindi_songs/Ishare_Tere.mp3',
+    cover: 'static/Hindi_songs/Ishare_Tere.jpg'
+  },
+  {
+    name: 'Jo Bheji Thi Duaa',
+    artist: 'Arijit Singh',
+    url: 'static/Hindi_songs/Jo_Bheji_Thi_Duaa.mp3',
+    cover: 'static/Hindi_songs/Jo_Bheji_Thi_Duaa.jpg'
+  },
+  {
+    name: 'Kabhi Jo Badal Barse',
+    artist: 'Arijit Singh',
+    url: 'static/Hindi_songs/Kabhi_Jo_Badal_Barse.mp3',
+    cover: 'static/Hindi_songs/Kabhi_Jo_Badal_Barse.jpg'
+  },
+  {
+    name: 'Kamariya',
+    artist: 'Aastha Gill, Divya Kumar and Sachin Sanghvi',
+    url: 'static/Hindi_songs/Kamariya.mp3',
+    cover: 'static/Hindi_songs/Kamariya.jpg'
+  },
+  {
+    name: 'Vaaste',
+    artist: 'Dhvani Bhanushali',
+    url: 'Hindi songs/Vaaste.mp3',
+    cover: 'Hindi songs/Vaaste.jpg'
+  },
+  {
+    name: 'Ishq tera',
+    artist: 'Guru Randhawa',
+    url: 'Hindi songs/Ishq_tera.mp3',
+    cover: 'Hindi songs/Ishq_tera.jpg'
+  },
+  {
+    name: 'Leja Re',
+    artist: 'Dhvani Bhanushali',
+    url: 'Hindi songs/Leja_Re.mp3',
+    cover: 'Hindi songs/Leja_Re.jpg'
+  },
+  {
+    name: 'Naina',
+    artist: '',
+    url: 'Hindi songs/Naina.mp3',
+    cover: 'Hindi songs/Naina.jpg'
+  },
+  {
+    name: 'Tera Ban Jaaunga',
+    artist: 'Akhil Sachdeva and Tulsi',
+    url: 'Hindi songs/Tera_Ban_Jaaunga.mp3',
+    cover: 'Hindi songs/Tera_Ban_Jaaunga.jpg'
+  },
+  {
+    name: 'Kabira',
+    artist: 'Rekha Bhardwaj and Tochi Raina',
+    url: 'Hindi songs/Kabira.mp3',
+    cover: 'Hindi songs/Kabira.jpg'
+  },
+  {
+    name: 'Teri Mitti',
+    artist: 'B Praak',
+    url: 'Hindi songs/Teri_Mitti.mp3',
+    cover: 'Hindi songs/Teri_Mitti.jpg'
+  },
+  {
+    name: 'Ve Maahi',
+    artist: 'Arijit Singh and Asees Kaur',
+    url: 'Hindi songs/Ve_Maahi.mp3',
+    cover: 'Hindi songs/Ve_Maahi.jpg'
+  },
+  {
+    name: 'Zara Zara Behekta Hai',
+    artist: 'Bombay Jayashri',
+    url: 'Hindi songs/Zara_Zara_Behekta_Hai.mp3',
+    cover: 'Hindi songs/Zara_Zara_Behekta_Hai.jpg'
+  },
+  {
+    name: 'Tera Fitoor',
+    artist: 'Arijit Singh',
+    url: 'Hindi songs/Tera_Fitoor.mp3',
+    cover: 'Hindi songs/Tera_Fitoor.jpg'
+  },
+  {
+    name: 'Tera Baa Aaya',
+    artist: 'Farhad Bhiwandiwala',
+    url: 'Hindi songs/Tera_Baa_Aaya.mp3',
+    cover: 'Hindi songs/Tera_Baa_Aaya.jpg'
+  },
+  {
+    name: 'Shankara Re Shankara',
+    artist: 'Mehul Vyas',
+    url: 'Hindi songs/Shankara_Re_Shankara.mp3',
+    cover: 'Hindi songs/Shankara_Re_Shankara.jpg'
+  },
+  {
+    name: 'Baala',
+    artist: 'Sohail Sen and Vishal Dadlani',
+    url: 'Hindi songs/Baala.mp3',
+    cover: 'Hindi songs/Baala.jpg'
+  },
+  {
+    name: 'O SAKI SAKI',
+    artist: 'Neha Kakkar and Tulsi Kumar',
+    url: 'Hindi songs/O_SAKI_SAKI.mp3',
+    cover: 'Hindi songs/O_SAKI_SAKI.jpg'
+  }
+  ]
 });
+
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value;
+  inputVal=inputVal.toLocaleLowerCase();
+  console.log("input event fired!", inputVal);
+  let songCards = document.getElementsByClassName("songCard");
+  Array.from(songCards).forEach(function (element) {
+    let songTxt = element.getElementsByTagName("h4")[0].innerText;
+    songTxt=songTxt.toLocaleLowerCase();
+    if (songTxt.includes(inputVal)) {
+      element.style.display = "block";
+    }
+    else {
+      element.style.display = "none";
+    }
+  })
+
+})
--- Marathi.js
+++ Marathi.js
@@ -12,212 +12,212 @@ $(".album-poster").on("click",function(){
   
   
   
-  const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-   listFolded:true,
-    audio: [{ 
-      name: 'A Savita Aga Savita',
-      artist: '',
-      url: 'Marathi songs/A Savita Aga Savita.mp3',
-      cover: 'Marathi songs/A Savita Aga Savita.jpg'
+const ap = new APlayer({
+container: document.getElementById('aplayer'),
+listFolded: true,
+audio: [{
+    name: 'A Savita Aga Savita',
+    artist: '',
+    url: 'static/Marathi_songs/A_Savita_Aga_Savita.mp3',
+    cover: 'static/Marathi_songs/A_Savita_Aga_Savita.jpg'
   },
-  { 
-      name: 'Aai Tujha deul.jpg',
-      artist: '',
-      url: 'Marathi songs/Aai Tujha deul.mp3',
-      cover: 'Marathi songs/Aai Tujha deul.jpg'
+  {
+    name: 'Aai Tujha deul.jpg',
+    artist: '',
+    url: 'static/Marathi_songs/Aai_Tujha_deul.mp3',
+    cover: 'static/Marathi_songs/Aai_Tujha_deul.jpg'
   },
-  { 
-      name: 'Aamdar Zalya Sarkha Vatatay',
-      artist: '',
-      url: 'Marathi songs/Aamdar Zalya Sarkha Vatatay.mp3',
-      cover: 'Marathi songs/Aamdar Zalya Sarkha Vatatay.jpg'
+  {
+    name: 'Aamdar Zalya Sarkha Vatatay',
+    artist: '',
+    url: 'static/Marathi_songs/Aamdar_Zalya_Sarkha_Vatatay.mp3',
+    cover: 'static/Marathi_songs/Aamdar_Zalya_Sarkha_Vatatay.jpg'
   },
-  { 
-      name: 'Aaple Saheb Thackrey',
-      artist:'',
-      url: 'Marathi songs/Aaple Saheb Thackrey.mp3',
-      cover: 'Marathi songs/Aaple Saheb Thackrey.jpg'
+  {
+    name: 'Aaple Saheb Thackrey',
+    artist: '',
+    url: 'static/Marathi_songs/Aaple_Saheb_Thackrey.mp3',
+    cover: 'static/Marathi_songs/Aaple_Saheb_Thackrey.jpg'
   },
-  { 
+  {
     name: 'Aatach Baya Ka Baavarla',
-    artist:'',
-    url: 'Marathi songs/Aatach Baya Ka Baavarla.mp3',
-    cover: 'Marathi songs/Aatach Baya Ka Baavarla.jpg'
+    artist: '',
+    url: 'static/Marathi_songs/Aatach_Baya_Ka_Baavarla.mp3',
+    cover: 'static/Marathi_songs/Aatach_Baya_Ka_Baavarla.jpg'
   },
-  { 
+  {
     name: 'Aho Mami Tumchi Mulagi Lay Sundar',
-    artist:'',
-    url: 'Marathi songs/Aho Mami Tumchi Mulagi Lay Sundar.mp3',
-    cover: 'Marathi songs/Aho Mami Tumchi Mulagi Lay Sundar.jpg'
+    artist: '',
+    url: 'static/Marathi_songs/Aho_Mami_Tumchi_Mulagi_Lay_Sundar.mp3',
+    cover: 'static/Marathi_songs/Aho_Mami_Tumchi_Mulagi_Lay_Sundar.jpg'
   },
-  { 
+  {
     name: 'Apsara Aali',
-    artist:'Sonalee Kulkarni and Ajay-Atul',
-    url: 'Marathi songs/Apsara Aali.mp3',
-    cover: 'Marathi songs/Apsara Aali.jpg'
+    artist: 'Sonalee Kulkarni and Ajay-Atul',
+    url: 'static/Marathi_songs/Apsara_Aali.mp3',
+    cover: 'static/Marathi_songs/Apsara_Aali.jpg'
   },
-  { 
+  {
     name: 'Bablya Ekas Kesavar Fuge',
     artist: '',
-    url: 'Marathi songs/Bablya Ekas Kesavar Fuge.mp3',
-    cover: 'Marathi songs/Bablya Ekas Kesavar Fuge.jpg'
+    url: 'static/Marathi_songs/Bablya_Ekas_Kesavar_Fuge.mp3',
+    cover: 'static/Marathi_songs/Bablya_Ekas_Kesavar_Fuge.jpg'
   },
-  { 
+  {
     name: 'Bhau - Shambu',
     artist: 'Shambu',
-    url: 'Marathi songs/Bhau - Shambu.mp3',
-    cover: 'Marathi songs/Bhau - Shambu.jpg'
+    url: 'static/Marathi_songs/Bhau-Shambu.mp3',
+    cover: 'static/Marathi_songs/Bhau-Shambu.jpg'
   },
-  { 
+  {
     name: 'Fandry Song',
     artist: 'Ajay-Atul',
-    url: 'Marathi songs/Fandry Song.mp3',
-    cover: 'Marathi songs/Fandry Song.jpg'
+    url: 'static/Marathi_songs/Fandry_Song.mp3',
+    cover: 'static/Marathi_songs/Fandry_Song.jpg'
   },
-  { 
+  {
     name: 'Gondhal',
     artist: 'Ajay-Atul',
-    url: 'Marathi songs/Gondhal.mp3',
-    cover: 'Marathi songs/Gondhal.jpg'
+    url: 'static/Marathi_songs/Gondhal.mp3',
+    cover: 'static/Marathi_songs/Gondhal.jpg'
   },
-  { 
+  {
     name: 'Govyachya Kinaryav',
     artist: '',
-    url: 'Marathi songs/Govyachya Kinaryav.mp3',
-    cover: 'Marathi songs/Govyachya Kinaryav.jpg'
+    url: 'static/Marathi_songs/Govyachya_Kinaryav.mp3',
+    cover: 'static/Marathi_songs/Govyachya_Kinaryav.jpg'
   },
-  { 
+  {
     name: 'Gulabachi Kali',
     artist: '',
-    url: 'Marathi songs/Gulabachi Kali.mp3',
-    cover: 'Marathi songs/Gulabachi Kali.jpg'
+    url: 'static/Marathi_songs/Gulabachi_Kali.mp3',
+    cover: 'static/Marathi_songs/Gulabachi_Kali.jpg'
   },
-  { 
+  {
     name: 'Hi Poli Saajuk',
     artist: 'Reshma Sonawane',
-    url: 'Marathi songs/Hi Poli Saajuk.mp3',
-    cover: 'Marathi songs/Hi Poli Saajuk.jpg'
+    url: 'static/Marathi_songs/Hi_Poli_Saajuk.mp3',
+    cover: 'static/Marathi_songs/Hi_Poli_Saajuk.jpg'
   },
-  { 
+  {
     name: 'Halad Lagali',
     artist: '',
-    url: 'Marathi songs/Halad Lagali.mp3',
-    cover: 'Marathi songs/Halad Lagali.jpg'
-  },
-  { 
+    url: 'static/Marathi_songs/Halad_Lagali.mp3',
+    cover: 'static/Marathi_songs/Halad_Lagali.jpg'
+  }, 
+  {
     name: 'Ishkkachi Nauka',
     artist: '',
-    url: 'Marathi songs/Ishkkachi Nauka.mp3',
-    cover: 'Marathi songs/Ishkkachi Nauka.jpg'
+    url: 'static/Marathi_songs/Ishkkachi_Nauka.mp3',
+    cover: 'static/Marathi_songs/Ishkkachi_Nauka.jpg'
   },
-  { 
+  {
     name: 'Ka Kalena',
     artist: '',
-    url: 'Marathi songs/Ka Kalena.mp3',
-    cover: 'Marathi songs/Ka Kalena.jpg'
+    url: 'static/Marathi_songs/Ka_Kalena.mp3',
+    cover: 'static/Marathi_songs/Ka_Kalena.jpg'
   },
-  { 
+  {
     name: 'Khel Mandala',
     artist: '',
-    url: 'Marathi songs/Khel Mandala.mp3',
-    cover: 'Marathi songs/Khel Mandala.jpg'
+    url: 'static/Marathi_songs/Khel_Mandala.mp3',
+    cover: 'static/Marathi_songs/Khel_Mandala.jpg'
   },
-  { 
+  {
     name: 'Koliwada Jhingla',
     artist: '',
-    url: 'Marathi songs/Koliwada Jhingla.mp3',
-    cover: 'Marathi songs/Koliwada Jhingla.jpg'
+    url: 'static/Marathi_songs/Koliwada_Jhingla.mp3',
+    cover: 'static/Marathi_songs/Koliwada_Jhingla.jpg'
   },
-  { 
+  {
     name: 'Kombadi Palali',
     artist: '',
-    url: 'Marathi songs/Kombadi Palali.mp3',
-    cover: 'Marathi songs/Kombadi Palali.jpg'
+    url: 'static/Marathi_songs/Kombadi_Palali.mp3',
+    cover: 'static/Marathi_songs/Kombadi_Palali.jpg'
   },
-  { 
+  {
     name: 'Mala Ved Lagale',
     artist: '',
-    url: 'Marathi songs/Mala Ved Lagale.mp3',
-    cover: 'Marathi songs/Mala Ved Lagale.jpg'
+    url: 'static/Marathi_songs/Mala_Ved_Lagale.mp3',
+    cover: 'static/Marathi_songs/Mala_Ved_Lagale.jpg'
   },
-  { 
+  {
     name: 'O kaka',
     artist: '',
-    url: 'Marathi songs/O kaka.mp3',
-    cover: 'Marathi songs/O kaka.jpg'
+    url: 'static/Marathi_songs/O_kaka.mp3',
+    cover: 'static/Marathi_songs/O_kaka.jpg'
   },
-  { 
+  {
     name: 'O Raje',
     artist: '',
-    url: 'Marathi songs/O Raje.mp3',
-    cover: 'Marathi songs/O Raje.jpg'
+    url: 'static/Marathi_songs/O_Raje.mp3',
+    cover: 'static/Marathi_songs/O_Raje.jpg'
   },
-  { 
+  {
     name: 'Pune RAP',
     artist: '',
-    url: 'Marathi songs/Pune RAP.mp3',
-    cover: 'Marathi songs/Pune RAP.jpg'
+    url: 'static/Marathi_songs/Pune_RAP.mp3',
+    cover: 'static/Marathi_songs/Pune_RAP.jpg'
   },
-  { 
+  {
     name: 'Saavar Re Mana',
     artist: '',
-    url: 'Marathi songs/Saavar Re Mana.mp3',
-    cover: 'Marathi songs/Saavar Re Mana.jpg'
+    url: 'static/Marathi_songs/Saavar_Re_Mana.mp3',
+    cover: 'static/Marathi_songs/Saavar_Re_Mana.jpg'
   },
-  { 
+  {
     name: 'Sairat Zala Ji',
     artist: 'Ajay-Atul',
-    url: 'Marathi songs/Sairat Zala Ji.mp3',
-    cover: 'Marathi songs/Sairat Zala Ji.jpg'
+    url: 'static/Marathi_songs/Sairat_Zala_Ji.mp3',
+    cover: 'static/Marathi_songs/Sairat_Zala_Ji.jpg'
   },
-  { 
+  {
     name: 'Shantabai',
     artist: '',
-    url: 'Marathi songs/Shantabia.mp3',
-    cover: 'Marathi songs/Shantabia.jpg'
+    url: 'static/Marathi_songs/Shantabia.mp3',
+    cover: 'static/Marathi_songs/Shantabia.jpg'
   },
-  { 
+  {
     name: 'Shitti Vajali',
     artist: '',
-    url: 'Marathi songs/Shitti Vajali.mp3',
-    cover: 'Marathi songs/Shitti Vajali.jpg'
+    url: 'static/Marathi_songs/Shitti_Vajali.mp3',
+    cover: 'static/Marathi_songs/Shitti_Vajali.jpg'
   },
-  { 
+  {
     name: 'TAN TAN BHAN BHAN',
     artist: 'Shambu',
-    url: 'Marathi songs/TAN TAN BHAN BHAN.mp3',
-    cover: 'Marathi songs/TAN TAN BHAN BHAN.jpg'
+    url: 'static/Marathi_songs/TAN_TAN_BHAN_BHAN.mp3',
+    cover: 'static/Marathi_songs/TAN_TAN_BHAN_BHAN.jpg'
   },
-  { 
+  {
     name: 'Tola Tola',
     artist: 'Bela Shende and Amitraj',
-    url: 'Marathi songs/Tola Tola.mp3',
-    cover: 'Marathi songs/Tola Tola.jpg'
+    url: 'static/Marathi_songs/Tola_Tola.mp3',
+    cover: 'static/Marathi_songs/Tola_Tola.jpg'
   },
-  { 
+  {
     name: 'Tu Chal Re Mana',
     artist: 'Virat M and Mahesh M',
-    url: 'Marathi songs/Tu Chal Re Mana.mp3',
-    cover: 'Marathi songs/Tu Chal Re Mana.jpg'
+    url: 'static/Marathi_songs/Tu_Chal_Re_Mana.mp3',
+    cover: 'static/Marathi_songs/Tu_Chal_Re_Mana.jpg'
   },
-  { 
+  {
     name: 'Wajle Ki Bara',
     artist: 'Amrita Khanvelkar and Ajay-Atul',
-    url: 'Marathi songs/Wajle Ki Bara.mp3',
-    cover: 'Marathi songs/Wajle Ki Bara.jpg'
+    url: 'static/Marathi_songs/Wajle_Ki_Bara.mp3',
+    cover: 'static/Marathi_songs/Wajle_Ki_Bara.jpg'
   },
-  { 
+  {
     name: 'Yad Lagla',
     artist: 'Ajay-Atul',
-    url: 'Marathi songs/Yad Lagla.mp3',
-    cover: 'Marathi songs/Yad Lagla.jpg'
+    url: 'static/Marathi_songs/Yad_Lagla.mp3',
+    cover: 'static/Marathi_songs/Yad_Lagla.jpg'
   },
-  { 
+  {
     name: 'Zingat',
     artist: 'Ajay-Atul',
-    url: 'Marathi songs/Zingat.mp3',
-    cover: 'Marathi songs/Zingat.jpg'
+    url: 'static/Marathi_songs/Zingat.mp3',
+    cover: 'static/Marathi_songs/Zingat.jpg'
   },
   { 
     name: 'Khulach Zalo Ga',
