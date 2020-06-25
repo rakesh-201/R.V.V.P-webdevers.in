37fb0ecc64dc46eec80311f8dda814e36dbc92b5
vdmondkr2002 <64477617+vdmondkr2002@users.noreply.github.com>
2020-06-26 01:06:00 +0530

    Updated again

--- Asha.js
+++ Asha.js
@@ -25,172 +25,178 @@ $(".album-poster").on("click", function () {
     audio: [ 
     {
       name: 'Deewana hua badal',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Kashmir Ki Kali',
       url: 'Asha_songs/Deewana_hua.mp3',
       cover: 'Asha_songs/Deewana_hua.jpg'
     },
     {
       name: 'Dhal gaya din',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Hamjoli',
       url: 'Asha_songs/Dhal_gaya.mp3',
       cover: 'Asha_songs/Dhal_gaya.jpg'
     },
     {
       name: 'Jaiye aap kaha jayenge',
-      artist: 'Asha Bhosale',
+      artist: 'Mere Sanam',
       url: 'Asha_songs/Jaiye_aap.mp3',
       cover: 'Asha_songs/Jaiye_aap.jpg'
     },
     {
       name: 'O saathi chal',
-      artist: 'Asha Bhosale, Kishore Kumar',
+      artist: 'Seeta Aur Geeta',
       url: 'Asha_songs/O_saathi.mp3',
       cover: 'Asha_songs/O_saathi.jpg'
     },
     {
       name: 'Piya tu ab to aaja',
-      artist: 'Asha Bhosale',
+      artist: 'Caravan',
       url: 'Asha_songs/Piya_tu.mp3',
       cover: 'Asha_songs/Piya_tu.jpg'
     },
     {
       name: 'Uden jab jab zulfen teri',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Naya Daur',
       url: 'Asha_songs/Uden_jab.mp3',
-      cover: 'Asha_songs/Uden_jab.jpg'
+      cover: 'Asha_songs/Uden_jab.png'
     },
     {
       name: 'Aaiye meharbaan',
-      artist: 'Asha Bhosale',
+      artist: 'Howrah Bridge',
       url: 'Asha_songs/Aaiye_meharbaan.mp3',
       cover: 'Asha_songs/Aaiye_meharbaan.jpg'
     },
     {
       name: 'Do lafzon ki hai',
-      artist: 'Asha Bhosale',
+      artist: 'The Great Gambler',
       url: 'Asha_songs/Do_lafzon.mp3',
       cover: 'Asha_songs/Do_lafzon.jpg'
     },
     {
       name: 'Ek mai aur ek tu',
-      artist: 'Asha Bhosale, Kishore Kumar',
+      artist: 'Khel Khel Mein',
       url: 'Asha_songs/Ek_mai.mp3',
       cover: 'Asha_songs/Ek_mai.jpg'
     },
     {
       name: 'Gun guna rahe hai bhanware',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Aaradhna',
       url: 'Asha_songs/Gun_guna.mp3',
       cover: 'Asha_songs/Gun_guna.jpg'
     },
     {
       name: 'Jab chali thandi hawa',
-      artist: 'Asha Bhosale',
+      artist: 'Kati Patang',
       url: 'Asha_songs/Jab_chali.mp3',
       cover: 'Asha_songs/Jab_chali.jpg'
     },
     {
       name: 'Sara pyar tumhara',
-      artist: 'Asha Bhosale, Kishore Kumar',
+      artist: 'Aanand Ashram',
       url: 'Asha_songs/Saara_pyar.mp3',
       cover: 'Asha_songs/Saara_pyar.jfif'
     },
     {
       name: 'Roka kai baar maine',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Mere Sanam',
       url: 'Asha_songs/Roka_kai.mp3',
       cover: 'Asha_songs/Roka_kai.jpg'
     },
     {
       name: 'Sar par topi laal',
-      artist: 'Asha Bhosale,Mohammad Rafi',
+      artist: 'Tumsa Nahi Dekha',
       url: 'Asha_songs/Sar_par.mp3',
       cover: 'Asha_songs/Sar_par.jpg'
     },
     {
       name: 'Raat ke hamsafar',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'An Evening in Paris',
       url: 'Asha_songs/Raat_ke.mp3',
       cover: 'Asha_songs/Raat_ke.jpg'
     },
     {
       name: 'Chhod do aanchal',
-      artist: 'Asha Bhosale, Kishore Kumar',
+      artist: 'Paying Guest',
       url: 'Asha_songs/Chhod_do.mp3',
       cover: 'Asha_songs/Chhod_do.jpg'
     },
     {
       name: 'Deewana mastana hua dil',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Bombai Ka Babu',
       url: 'Asha_songs/Deewana_mastana.mp3',
       cover: 'Asha_songs/Deewana_mastana.jpg'
     },
     {
       name: 'Ek pardesi mera dil le gaya',
-      artist: 'Asha Bhosale, Mohammad Rafi',
+      artist: 'Phagun',
       url: 'Asha_songs/Ek_pardesi.mp3',
       cover: 'Asha_songs/Ek_pardesi.jpg'
     },
     {
       name: 'Yeh mera dil',
-      artist: 'Asha Bhosale',
+      artist: 'Don',
       url: 'Asha_songs/Ye_mera.mp3',
       cover: 'Asha_songs/Ye_mera.jpg'
     },
     {
       name: 'Ha tu hai wahi',
-      artist: 'Asha Bhosle, Kishore Kumar',
+      artist: 'Yeh Waada Raha',
       url: 'Kishore_songs/Ha_tu_hai.mp3',
       cover: 'Kishore_songs/Ha_tu_hai.jpeg'
     },
     {
       name: 'Lekar hu deewana dil',
-      artist: 'Asha Bhosle, Kishore Kumar',
+      artist: 'Yaadon Ki Baarat',
       url: 'Asha_songs/Lekar_hum.mp3',
       cover: 'Asha_songs/Lekar_hum.jpeg'
     },
     {
       name: 'O haseena',
-      artist: 'Asha Bhosle, Mohammad Rafi',
+      artist: 'Teesri Manzil',
       url: 'Asha_songs/O_haseena.mp3',
       cover: 'Asha_songs/O_haseena.jpg'
     },
     {
       name: 'Ye ladka haye allah',
-      artist: 'Asha Bhosle, Mohammad Rafi',
+      artist: 'Hum kisise kum Nahin',
       url: 'Asha_songs/Ye_ladka.mp3',
       cover: 'Asha_songs/Ye_ladka.jpg'
     },
     {
       name: 'Dil cheez kya hai',
-      artist: 'Asha Bhosle',
+      artist: 'Umarao Jaan',
       url: 'Asha_songs/Dil_cheez.mp3',
       cover: 'Asha_songs/Dil_cheez.jpeg'
     },
     {
       name: 'Aao na gale laga lo na',
-      artist: 'Asha Bhosle',
+      artist: 'Mere Jeevan Saathi',
       url: 'Asha_songs/Aao_na.mp3',
       cover: 'Asha_songs/Aao_na.jpg'
     },
     {
       name: 'Pyar ka dard hai',
-      artist: 'Asha Bhosle, Kishore Kumar',
+      artist: 'Dard',
       url: 'Asha_songs/Pyar_ka.mp3',
       cover: 'Asha_songs/Pyar_ka.jpg'
     },
     {
       name: 'In aankhon ki masti ke',
-      artist: 'Asha Bhosle',
+      artist: 'Umarao jaan',
       url: 'Asha_songs/In_aankhon.mp3',
       cover: 'Asha_songs/In_aankhon.png'
     },
     {
       name: 'Jab chhaye mera jadu',
-      artist: 'Asha Bhosle',
+      artist: 'Lootmaar',
       url: 'Asha_songs/Jab_chhaye.mp3',
       cover: 'Asha_songs/Jab_chhaye.jpg'
     },
+    {
+      name: 'Mang ke saath tumhara',
+      artist: 'Asha Bhosle',
+      url: 'Asha_songs/Maang_ke.mp3',
+      cover: 'Asha_songs/Maang_ke.jpg'
+    }
   ]
   });
   
