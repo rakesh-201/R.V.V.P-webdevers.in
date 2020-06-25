b6d5c3a65bb59838e0e55015f851ca5c177ff3c2
vdmondkr2002 <64477617+vdmondkr2002@users.noreply.github.com>
2020-06-26 01:09:04 +0530

    Updated again

--- Kishore.js
+++ Kishore.js
@@ -24,175 +24,175 @@ const ap = new APlayer({
   audio: [
     {
       name: 'Pal pal dil ke paas',
-      artist: 'Kishore Kumar',
+      artist: 'Blackmail',
       url: 'Kishore_songs/Pal_pal.mp3',
       cover: 'Kishore_songs/Pal_pal.jpg'
     },
     {
       name: 'Yeh shaam mastani',
-      artist: 'Kishore Kumar',
+      artist: 'Kati Patang',
       url: 'Kishore_songs/Yeh_shaam.mp3',
       cover: 'Kishore_songs/Yeh_shaam.jpg'
     },
     {
       name: 'O mere dil ke chain',
-      artist: 'Kishore Kumar',
+      artist: 'Mere Jeevan Saathi',
       url: 'Kishore_songs/O_mere.mp3',
       cover: 'Kishore_songs/O_mere.jpg'
     },
     {
       name: 'Tere bina Zindagi se koi',
-      artist: 'Kishore Kumar,Lata Mangeshkar',
+      artist: 'Aandhi',
       url: 'Kishore_songs/Tere_bina.mp3',
       cover: 'Kishore_songs/Tere_bina.jpg'
     },
     {
       name: 'Raat kali ek khwab me',
-      artist: 'Kishore Kumar',
+      artist: 'Buddha Mil Gaya',
       url: 'Kishore_songs/Raat_kali.mp3',
       cover: 'Kishore_songs/Raat_kali.jpg'
     },
     {
       name: 'O meri soni meri tamanna',
-      artist: 'Kishore Kumar,Asha Bhosale',
+      artist: 'Yaadon Ki Barat',
       url: 'Kishore_songs/Meri_soni.mp3',
       cover: 'Kishore_songs/Meri_soni.png'
     },
     {
       name: 'Neele neele ambar par',
-      artist: 'Kishore Kumar',
+      artist: 'Kalaakar',
       url: 'Kishore_songs/Neele_neele.mp3',
       cover: 'Kishore_songs/Neele_neele.jpg'
     },
     {
       name: 'Kehdu tumhe ya chup rahu',
-      artist: 'Kishore Kumar, Asha Bhosle',
+      artist: 'Deewar',
       url: 'Kishore_songs/Kehdu_tumhe.mp3',
       cover: 'Kishore_songs/Kehdu_tumhe.jpg'
     },
     {
       name: 'Khaike paan banaras wala',
-      artist: 'Kishore Kumar',
+      artist: 'Don',
       url: 'Kishore_songs/Khaike_paan.mp3',
       cover: 'Kishore_songs/Khaike_paan.jpg'
     },
     {
       name: 'Kora kagaz tha ye man mera',
-      artist: 'Kishore Kumar, Lata Mangeshkar',
+      artist: 'Aaradhna',
       url: 'Kishore_songs/Kora_kagaz.mp3',
       cover: 'Kishore_songs/Kora_kagaz.jpg'
     },
     {
       name: 'Mere mehboob quyamat hogi',
-      artist: 'Kishore Kumar',
+      artist: 'Mr.X in Bombay',
       url: 'Kishore_songs/Mere_mehboob.mp3',
       cover: 'Kishore_songs/Mere_mehboob.jpg'
     },
     {
       name: 'Mere sapno ki rani',
-      artist: 'Kishore Kumar',
+      artist: 'Aaradhna',
       url: 'Kishore_songs/Mere_sapno.mp3',
-      cover: 'Kishore songs/Mere sapno.jpg'
+      cover: 'Kishore_songs/Mere_sapno.jpg'
     },
     {
       name: 'O saathi chal',
-      artist: 'Kishore Kumar, Asha Bhosale',
+      artist: 'Seeta Aur Geeta',
       url: 'Kishore_songs/O_saathi.mp3',
       cover: 'Kishore_songs/O_saathi.jpg'
     },
     {
       name: 'Chhod do aanchal',
-      artist: 'Kishore Kumar, Asha Bhosale',
+      artist: 'Paying Guest',
       url: 'Kishore_songs/Chhod_do.mp3',
       cover: 'Kishore_songs/Chhod_do.jpg'
     },
     {
       name: 'Dil kya kare',
-      artist: 'Kishore Kumar',
+      artist: 'Julee',
       url: 'Kishore_songs/Dil_kya_kare.mp3',
       cover: 'Kishore_songs/Dil_kya_kare.jpg'
     },
     {
       name: 'Gaata rahe mera dil',
-      artist: 'Kishore Kumar, Lata Mangeshkar',
+      artist: 'Guide',
       url: 'Kishore_songs/Gaata_rahe.mp3',
       cover: 'Kishore_songs/Gaata_rahe.jpg'
     },
     {
       name: 'Gham ka fasana',
-      artist: 'Kishore Kumar',
+      artist: 'Manchali',
       url: 'Kishore_songs/Gham_ka.mp3',
       cover: 'Kishore_songs/Gham_ka.jpg'
     },
     {
       name: 'Gum hai kisi ke',
-      artist: 'Kishore Kumar, Lata Mangeshkar',
+      artist: 'Rampur Ka Lakshman',
       url: 'Kishore_songs/Gum_hai.mp3',
       cover: 'Kishore_songs/Gum_hai.jpg'
     },
     {
       name: 'Jai jai shiv shankar',
-      artist: 'Kishore Kumar, Lata Mangeshkar',
+      artist: 'Aap Ki kasam',
       url: 'Kishore_songs/Jai_jai.mp3',
       cover: 'Kishore_songs/Jai_jai.jpg'
     },
     {
       name: 'Rimjhim gire saavan',
-      artist: 'Kishore Kumar, Lata Mangeshkar',
+      artist: 'Manzil',
       url: 'Kishore_songs/Rimjhim_gire.mp3',
       cover: 'Kishore_songs/Rimjhim_gire.jpg'
     },
     {
       name: 'Sara zamana haseeno ka deewana',
-      artist: 'Kishore Kumar',
+      artist: 'Yaarana',
       url: 'Kishore_songs/Sara_zamana.mp3',
       cover: 'Kishore_songs/Sara_zamana.jpg'
     },
     {
       name: 'Tum aa gaye ho',
-      artist: 'Kishore Kumar, Lata mageshkar',
+      artist: 'Aandhi',
       url: 'Kishore_songs/Tum_aa.mp3',
       cover: 'Kishore_songs/Tum_aa.jpg'
     },
     {
       name: 'Wada tera wada',
-      artist: 'Kishore Kumar',
+      artist: 'Dushman',
       url: 'Kishore_songs/Wada_tera.mp3',
       cover: 'Kishore_songs/Wada_tera.jpg'
     },
     {
       name: 'Ek mai aur ek tu',
-      artist: 'Asha Bhosale, Kishore Kumar',
+      artist: 'Khel Khel Mein',
       url: 'Asha_songs/Ek_mai.mp3',
       cover: 'Asha_songs/Ek_mai.jpg'
     },
     {
       name: 'Suno kaho suna',
-      artist: 'Kishore Kumar. Lata MAngeshkar',
+      artist: 'Aap Ki Kasam',
       url: 'Lata_Songs/Suno_kaho.mp3',
       cover: 'Lata_Songs/Suno_kaho.jpeg'
     },
     {
       name: 'Bheegi bheegi raaton me',
-      artist: 'Kishore Kumar, Lata Mangeshkar',
+      artist: 'Ajanbee',
       url: 'Lata_Songs/Bheegi_bheegi.mp3',
       cover: 'Lata_Songs/Bheegi_bheegi.jpeg'
     },
     {
       name: 'Sagar jaisi aankhovali',
-      artist: 'Kishore Kumar',
+      artist: 'Sagar',
       url: 'Kishore_songs/Sagar_jaisi.mp3',
       cover: 'Kishore_songs/Sagar_jaisi.jpg'
     },
     {
       name: 'aise na mujhe tum dekho',
-      artist: 'Kishore Kumar',
+      artist: 'Darling Darling',
       url: 'Kishore_songs/Aise_na.mp3',
       cover: 'Kishore_songs/Aise_na.jpg'
     },
     {
       name: 'Ha tu hai wahi',
-      artist: 'Kishore Kumar',
+      artist: 'Yeh Wada Raha',
       url: 'Kishore_songs/Ha_tu_hai.mp3',
       cover: 'Kishore_songs/Ha_tu_hai.jpeg'
     },
@@ -254,4 +254,3 @@ function changeIcon(element){
     }
     
 }
-
