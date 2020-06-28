4e14bcfdaa86932234c43dca6e418f67ff06fe51
vdmondkr2002 <64477617+vdmondkr2002@users.noreply.github.com>
2020-06-28 16:21:32 +0530

    Added favorites

--- Kishore.js
+++ Kishore.js
@@ -196,8 +196,31 @@ const ap = new APlayer({
       url: 'Kishore_songs/Ha_tu_hai.mp3',
       cover: 'Kishore_songs/Ha_tu_hai.jpeg'
     },
+    {
+      name: 'Pyar ka dard hai',
+      artist: 'Dard',
+      url: 'Asha_songs/Pyar_ka.mp3',
+      cover: 'Asha_songs/Pyar_ka.jpg'
+    }
   ]
 });
+
+console.log(ap.audio);
+let audiofiles=[
+
+      'Kishore_songs/Pal_pal.mp3','Kishore_songs/Yeh_shaam.mp3',
+      'Kishore_songs/O_mere.mp3','Kishore_songs/Tere_bina.mp3','Kishore_songs/Raat_kali.mp3',
+      'Kishore_songs/Meri_soni.mp3','Kishore_songs/Neele_neele.mp3','Kishore_songs/Kehdu_tumhe.mp3',
+      'Kishore_songs/Khaike_paan.mp3','Kishore_songs/Kora_kagaz.mp3','Kishore_songs/Mere_mehboob.mp3',
+      'Kishore_songs/Mere_sapno.mp3','Kishore_songs/O_saathi.mp3','Kishore_songs/Chhod_do.mp3',
+      'Kishore_songs/Dil_kya_kare.mp3','Kishore_songs/Gaata_rahe.mp3','Kishore_songs/Gham_ka.mp3',
+      'Kishore_songs/Gum_hai.mp3','Kishore_songs/Jai_jai.mp3','Kishore_songs/Rimjhim_gire.mp3',
+      'Kishore_songs/Sara_zamana.mp3','Kishore_songs/Tum_aa.mp3','Kishore_songs/Wada_tera.mp3',
+      'Asha_songs/Ek_mai.mp3','Lata_Songs/Suno_kaho.mp3','Lata_Songs/Bheegi_bheegi.mp3',
+      'Kishore_songs/Sagar_jaisi.mp3','Kishore_songs/Aise_na.mp3','Kishore_songs/Ha_tu_hai.mp3',
+      'Asha_songs/Pyar_ka.mp3'
+];   
+
 
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", function () {
@@ -254,3 +277,76 @@ function changeIcon(element){
     }
     
 }
+
+let songCards = document.getElementsByClassName("songCard");
+var myFavprop = JSON.parse(localStorage.getItem("favProp"));
+// console.log(myFavprop);
+Array.from(songCards).forEach((e)=>{
+  let songCard = e.childNodes;
+  let songName = songCard[3].childNodes[1].innerHTML;
+  if (myFavprop.some(e => e.songName === songName)) {
+    /*localStorage contains this song*/
+    console.log(songCard[7].childNodes[0]);
+    let favBtn  = songCard[7].childNodes[0];
+    console.log("Both are same");
+    console.log(favBtn);
+    favBtn.classList = "fa fa-heart";
+  }
+
+})
+ 
+
+$(function () {
+  $(".favBtn").on("click", function (e) {
+    var icon = e.target;
+    var songCard = this.parentNode.childNodes;
+    var imgFile = songCard[1].childNodes[1].src;
+    var songName = songCard[3].childNodes[1].innerHTML;
+    var songInfo = songCard[5].childNodes[0].innerHTML;
+    var dataId = $(songCard[1]).attr("data-switch");
+    var audioFile = audiofiles[dataId];
+    console.log(audioFile);
+    var propToAdd = {
+      imgFile: imgFile,
+      songName: songName,
+      songInfo: songInfo,
+      audioFile: audioFile
+    }
+    var myFavprop = JSON.parse(localStorage.getItem("favProp"));
+    console.log(propToAdd);
+    console.log(myFavprop);
+    if (icon.classList == "fa fa-heart") {    
+      
+      if (myFavprop == null) {
+        myFavprop = [];
+      }
+      console.log(myFavprop);
+      if(myFavprop != null){
+         if(myFavprop.some(e => e.songName === propToAdd.songName)){
+           console.log("already included");
+            if(propToAdd == myFavprop[j]){
+               propToAdd = {};
+            }
+         }else{
+          console.log("pushing");
+          myFavprop.push(propToAdd);
+         }
+      }
+      localStorage.setItem("favProp", JSON.stringify(myFavprop));
+    }
+    if(icon.classList == "fa fa-heart-o"){
+      for(let j=0;j<myFavprop.length;j++){
+        if((myFavprop[j].songName == propToAdd.songName)&&(myFavprop[j].songInfo == propToAdd.songInfo)){
+          console.log("already included");
+          myFavprop.splice(j,1);
+        }
+        console.log(myFavprop);
+      }
+      localStorage.setItem("favProp", JSON.stringify(myFavprop));
+    }
+
+  })
+})
+
+
+
