7d7282f678075ca23d66df61980aa90e15d4ee76
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2020-06-30 17:08:52 +0530

    Add files via upload

--- 2019.js
+++ 2019.js
@@ -109,6 +109,14 @@ $(".album-poster").on("click", function () {
 
   ]
   });
+  let audiofiles = [
+    'Vishal/2019/Kinna_Sona.mp3','Vishal/2019/Garmi.mp3','Vishal/2019/Teri_Mitti.mp3','Vishal/2019/Hawa_Banke.mp3',
+    'Vishal/2019/O_Saki_Saki.mp3','Vishal/2020/Makhna.mp3','Vishal/2019/Chandigarh_Mein.mp3','Vishal/2019/Tujhe_Kitna_Chahne_Lage.mp3',
+    'Vishal/2019/Thodi_Jagah.mp3','Vishal/2019/Psycho_Saiyaan.mp3','Vishal/arjit/Pachtaoge.mp3',
+    'Vishal/arjit/Tu_Mila_Toh_Haina.mp3', 'Vishal/arjit/Ik_Vaari_ya.mp3','Vishal/arjit/Darkhaast.mp3',
+ 
+
+  ];
   let search = document.getElementById("searchTxt");
   search.addEventListener("input", function () {
     let inputVal = search.value;
@@ -142,7 +150,88 @@ $(".album-poster").on("click", function () {
         element.style.color ="lightgreen";
       })
 
+      function changeIcon(element){
+        let icon = element.childNodes[0];
+        if(icon.classList == "fa fa-heart-o"){
+          icon.classList ="fa fa-heart";
+        }
+        else{
+          icon.classList = "fa fa-heart-o";
+        }
+        
+    }
     
+let songCards = document.getElementsByClassName("songCard");
+var myFavprop = JSON.parse(localStorage.getItem("favProp"));
+// console.log(myFavprop);
+Array.from(songCards).forEach((e) => {
+  let songCard = e.childNodes;
+  let songName = songCard[3].childNodes[1].innerHTML;
+  if (myFavprop.some(e => e.songName === songName)) {
+    /*localStorage contains this song*/
+    console.log(songCard[7].childNodes[0]);
+    let favBtn = songCard[7].childNodes[0];
+    console.log("Both are same");
+    console.log(favBtn);
+    favBtn.classList = "fa fa-heart";
+  }
+
+})
+
+
+$(function () {
+  $(".favBtn").on("click", function (e) {
+    var icon = e.target;
+    var songCard = this.parentNode.childNodes;
+    var imgFile = songCard[1].childNodes[1].src;
+    var songName = songCard[3].childNodes[1].innerHTML;
+    var songInfo = songCard[5].childNodes[0].innerHTML;
+    var dataId = $(songCard[1]).attr("data-switch");
+    var audioFile = audiofiles[dataId];
+    console.log(audioFile);
+    var propToAdd = {
+      imgFile: imgFile,
+      songName: songName,
+      songInfo: songInfo,
+      audioFile: audioFile
+    }
+    var myFavprop = JSON.parse(localStorage.getItem("favProp"));
+    console.log(propToAdd);
+    console.log(myFavprop);
+    if (icon.classList == "fa fa-heart") {
+
+      if (myFavprop == null) {
+        myFavprop = [];
+      }
+      console.log(myFavprop);
+      if (myFavprop != null) {
+        if (myFavprop.some(e => e.songName === propToAdd.songName)) {
+          console.log("already included");
+          if (propToAdd == myFavprop[j]) {
+            propToAdd = {};
+          }
+        } else {
+          console.log("pushing");
+          myFavprop.push(propToAdd);
+        }
+      }
+      localStorage.setItem("favProp", JSON.stringify(myFavprop));
+    }
+    if (icon.classList == "fa fa-heart-o") {
+      for (let j = 0; j < myFavprop.length; j++) {
+        if ((myFavprop[j].songName == propToAdd.songName) && (myFavprop[j].songInfo == propToAdd.songInfo)) {
+          console.log("already included");
+          myFavprop.splice(j, 1);
+        }
+        console.log(myFavprop);
+      }
+      localStorage.setItem("favProp", JSON.stringify(myFavprop));
+    }
+
+  })
+});
+  
+  
 
       
 
